aa94466b44ca15417da37b2b5612a1dffdd8c142
beatrizmendoncadeoliveira <47859767+beatrizmendoncadeoliveira@users.noreply.github.com>
2019-04-11 09:16:48 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,36 @@
+var titulo=document.querySelector("#titulo");
+titulo.textContent="TABELA EX 3";
+
+var cor=document.querySelector("#branca1");
+cor.style.background="white";
+
+var cor2=document.querySelector("#cinza1");
+cor2.style.background="grey";
+
+var cor3=document.querySelector("#cinza2");
+cor3.style.background="grey";
+
+var cor4=document.querySelector("#cinza3");
+cor4.style.background="grey";
+
+var cor5=document.querySelector("#cinza4");
+cor5.style.background="grey";
+
+var cor6=document.querySelector("#cinza5");
+cor6.style.background="grey";
+
+var cor7=document.querySelector("#cinza6");
+cor7.style.background="grey";
+
+var cor8=document.querySelector("#cinza7");
+cor8.style.background="grey";
+
+var cor9=document.querySelector("#cinza8");
+cor9.style.background="grey";
+
+var cor10=document.querySelector("#cinza9");
+cor10.style.background="grey";
+
+var cor11=document.querySelector("#cinza10");
+cor11.style.background="grey";
+
